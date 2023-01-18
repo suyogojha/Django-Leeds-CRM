f3d8bc5941b75be95ad9d414af2db9b9ea97b85e
suyog ojha <ojhasuyog2020@gmail.com>
2023-01-18 19:50:29 +0545

    @Inviting Agents

--- agents/views.py
+++ agents/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
 from .forms import AgentModelForm
 from .mixins import OrganisorAndLoginRequiredMixins
-
+from django.core.mail import send_mail
+import random
 class AgentListView(OrganisorAndLoginRequiredMixins, generic.ListView):
     template_name = "agents/agent_list.html"
     
@@ -23,9 +24,21 @@ class AgentCreateView(OrganisorAndLoginRequiredMixins, generic.CreateView):
         return reverse("agents:agent-list")
     
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organization = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organization = self.request.user.userprofile
+        )
+        send_mail(
+            subject = "You are invited to be an agent",
+            message="You're added as an agent.Please login to procide further. ",
+            from_email= "admin@admin.com",
+            recipient_list=[user.email]
+        )
         return super(AgentCreateView, self).form_valid(form)
     
     
